11ce423536eda14fc8f3288fceb02e6ac8d0311f
Osvaldo <osv_93@hotmail.es>
2021-03-24 20:51:13 -0600

    implent JWT and add new middleware  to validate a valid JWT

---
+++ Appointment.API/Appointment.API/Models/AuthenticateResponseDto.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Appointment.API.Models
+{
+    public class AuthenticateResponseDto
+    {
+        public string Token { get; set; }
+
+        public AuthenticateResponseDto(string token)
+        {
+            Token = token;
+        }
+    }
+}
---
+++ Appointment.API/Appointment.API/Services/SecurityService.cs
@@ -0,0 +1,61 @@
+using Appointment.API.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WebApi.Helpers;
+
+namespace WebApi.Services
+{
+    public interface ISecurityService
+    {
+        AuthenticateResponseDto Authenticate(AuthenticateRequestDto model);
+    }
+
+    public class SecurityService : ISecurityService
+    {
+        // users hardcoded for simplicity, store in a db with hashed passwords in production applications
+        //private List<User> _users = new List<User>
+        //{
+        //    new User { Id = 1, FirstName = "Test", LastName = "User", Username = "test", Password = "test" }
+        //};
+
+        private readonly AppSettings _appSettings;
+
+        public SecurityService(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+
+        public AuthenticateResponseDto Authenticate(AuthenticateRequestDto model)
+        {
+            //var user = _users.SingleOrDefault(x => x.Username == model.Username && x.Password == model.Password);
+            var user = "secret";
+
+            // return null if user not found
+            if (user == null) return null;
+
+            // authentication successful so generate jwt token
+            var token = generateJwtToken(user);
+
+            return new AuthenticateResponseDto(token);
+        }
+
+        private string generateJwtToken(string id)
+        {
+            // generate token that is valid for 7 days
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", id) }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
